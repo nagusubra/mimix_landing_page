ecebc985bab98c9e5c0c4747176cb18c525a0b3f
Subramanian <52630559+nagusubra@users.noreply.github.com>
2025-06-01 17:34:01 -0600

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Add your Next.js configuration options here
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default nextConfig;
